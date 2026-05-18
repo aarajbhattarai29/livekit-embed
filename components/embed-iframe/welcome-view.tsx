ffae1b56f761182dc03cb64e3045cd73d80cfc57
Ubuntu <azureuser@livekit-server.lfzmboxb4ptezmajvny5n3othg.rx.internal.cloudapp.net>
2026-05-18 09:28:21 +0000

    Welcome Branding Modified

--- components/embed-iframe/welcome-view.tsx
+++ components/embed-iframe/welcome-view.tsx
@@ -22,9 +22,9 @@ export const WelcomeView = ({
       <div className="flex h-full items-center justify-between gap-4 px-3">
         <div className="pl-3">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
+          {/* <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={logoDark} alt={`${companyName} Logo`} className="hidden size-6 dark:block" />
+          {/* <img src={logoDark} alt={`${companyName} Logo`} className="hidden size-6 dark:block" />  */}
         </div>
 
         <Button variant="primary" size="lg" onClick={onStartCall} className="w-48 font-mono">
